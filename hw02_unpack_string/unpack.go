968ce71dc6a78ec6b6687f4c5608664ff57c7fd2
ajdaha <r.tarisov@gmail.com>
2025-11-26 10:54:01 +0500

    feat: task hw02_unpack_string resolved and fix tests

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var b strings.Builder
+	var prev rune
+	var isEscaped bool
+
+	for _, cur := range s {
+		switch {
+		case cur == '\\' && !isEscaped:
+			isEscaped = true
+		case unicode.IsDigit(cur) && !isEscaped:
+			if prev == 0 {
+				return "", ErrInvalidString
+			}
+			count := int(cur - '0')
+			for i := 0; i < count; i++ {
+				b.WriteRune(prev)
+			}
+			prev = 0
+		default:
+			if prev != 0 {
+				b.WriteRune(prev)
+			}
+			prev = cur
+			isEscaped = false
+		}
+	}
+
+	if prev != 0 {
+		if isEscaped {
+			return "", ErrInvalidString
+		}
+		b.WriteRune(prev)
+	}
+
+	return b.String(), nil
 }
