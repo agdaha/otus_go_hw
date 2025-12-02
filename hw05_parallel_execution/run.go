1b7c57f9618d7961ffe18ad85b2d2246374aa9dc
ajdaha <r.tarisov@gmail.com>
2025-12-02 16:28:17 +0500

    feat: task hw05_parallel_execution resolved

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,9 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +13,63 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if n <= 0 {
+		return fmt.Errorf("the number of workers cannot be less than or equal to 0")
+	}
+
+	if len(tasks) == 0 {
+		return nil
+	}
+
+	ignoreErrorLimit := m <= 0 // игнорировать лимит ошибок если он <= 0
+	errLimit := int64(m)       // количество ошибок, которые можно пропустить, приведено в int64 для атомикова
+
+	taskCh := make(chan Task)
+	cancel := make(chan struct{})
+
+	var wg sync.WaitGroup
+	wg.Add(n)
+
+	var isCancelled int32 // статус канала cancel 0 = в работе, 1 = остановлен
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range taskCh {
+				select {
+				case <-cancel:
+					return
+				default:
+					err := task()
+					if err != nil {
+						atomic.AddInt64(&errLimit, -1)
+					}
+					if atomic.LoadInt64(&errLimit) <= 0 && !ignoreErrorLimit {
+						if atomic.CompareAndSwapInt32(&isCancelled, 0, 1) {
+							close(cancel)
+						}
+						return
+					}
+				}
+			}
+		}()
+	}
+
+pusher:
+	for _, task := range tasks {
+		select {
+		case <-cancel:
+			break pusher
+		case taskCh <- task:
+		}
+	}
+	close(taskCh)
+
+	wg.Wait()
+
+	if errLimit <= 0 && !ignoreErrorLimit {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
